caceed2cbcb41ea9c78f997839fe95709fb3e9a5
sanm <sanmzhang@163.com>
2025-07-24 20:29:11 +0800

    finish sleep

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+
+int 
+main(int argc, char* argv[]) {
+    if(argc != 2) {
+        fprintf(2, "Usage: sleep times\n");
+        exit(1);
+    }
+    int time = atoi(argv[1]);
+    if (sleep(time) != 0) {
+        fprintf(2, "Error in sleep sys_call!\n");
+        exit(1);
+    }
+    exit(0);
+}
